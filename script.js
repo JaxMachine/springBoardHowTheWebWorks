fc2738d3b106aba7885467c80209f164c2edddc9
JaxMachine <newtradesofjack@gmail.com>
2024-02-29 12:01:57 -0600

    Project Completed

---
+++ script.js
@@ -0,0 +1,50 @@
+/*
+
+1. What is HTTP?
+
+It stands for Hyper Text Transfer Protocol, which determines how clients and server's 
+communitcate through a network.
+
+2. What is URL?
+
+URL stands for Uniform Resource Locator, which is the location of some internet resource.
+
+3. What is DNS?
+
+Stands for Domain Name System. It turns human readible URLs into IP Addresses.
+
+4. What is a Query String?
+
+Allows key-value pairs to be added to the URL. Promper Format "?firstKey=value1&secondKey=value2"
+
+5. List two HTTP verbs
+
+    GET - get data from a server
+    POST - send data to a server
+
+6. What is an HTTP Request?
+
+    A request from a client computer to a server that uses the HTTP protocol. 
+
+7. What is an HTTP Response?
+
+    A response from a server to a client that uses the HTTP protocol.
+
+8. What is an HTTP Header?
+
+    HTTP Headers provide additional information about the request/response. 
+
+    Request Headers: Host, Accept, Cookie
+
+    Response Headers: Content Type, Last-Modified.
+
+
+9. What happens you type a URL in a browser?
+
+    -The URL is resolved using DNS into an IP address.
+    -The IP addess is used to make a request by your browser.
+    -The server recieves that request and returns the response.
+    -Your browser formats the response appropriately - such as displaying HTML correctly and loading in any
+         additional resources
+
+*/
